e92b6ee5cac67e9535856b2bc8f392310387697e
xgd506 <119094827+xgd506@users.noreply.github.com>
2023-08-05 10:25:47 +0800

    网关

---
+++ service-gateway/src/main/java/hue/xgd/ttyx/gateway/ServiceGatewayApplication.java
@@ -0,0 +1,16 @@
+package hue.xgd.ttyx.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:xgd
+ * @Date:2023/8/4 19:45
+ * @Description:
+ */
+@SpringBootApplication
+public class ServiceGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class,args);
+    }
+}
---
+++ service-gateway/src/main/java/hue/xgd/ttyx/gateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package hue.xgd.ttyx.gateway.config;
+
+/**
+ * @Author:xgd
+ * @Date:2023/8/4 19:46
+ * @Description:
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
